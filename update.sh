3f79c601e71b721fcd1372fe3de2fe1a557878d1
hack4lx <38162851+attakercyebr@users.noreply.github.com>
2020-03-19 17:28:41 +0000

    Add files via upload

---
+++ update.sh
@@ -0,0 +1,59 @@
+#
+# __                            __        __    __  __           
+#/  |                          /  |      /  |  /  |/  |          
+#$$ |____    ______    _______ $$ |   __ $$ |  $$ |$$ | __    __ 
+#$$      \  /      \  /       |$$ |  /  |$$ |__$$ |$$ |/  \  /  |
+#$$$$$$$  | $$$$$$  |/$$$$$$$/ $$ |_/$$/ $$    $$ |$$ |$$  \/$$/ 
+#$$ |  $$ | /    $$ |$$ |      $$   $$<  $$$$$$$$ |$$ | $$  $$<  
+#$$ |  $$ |/$$$$$$$ |$$ \_____ $$$$$$  \       $$ |$$ | /$$$$  \ 
+#$$ |  $$ |$$    $$ |$$       |$$ | $$  |      $$ |$$ |/$$/ $$  |
+#$$/   $$/  $$$$$$$/  $$$$$$$/ $$/   $$/       $$/ $$/ $$/   $$/ 
+#👊 ʍ4ղíƒҽՏԵ0 ϲվҍҽɾ ՏҽϲմɾíԵվ Եҽɑʍ™💪              
+#Tool For Hacking 
+#Authors : hack4lx
+
+clear
+
+sudo chmod +x /etc/
+
+clear
+
+sudo chmod +x /usr/share/doc
+
+clear
+
+sudo rm -rf /usr/share/doc/Crips/
+
+clear
+
+cd /etc/
+
+clear
+
+sudo rm -rf /etc/Manisso
+
+clear
+
+mkdir Manisso
+
+clear
+
+cd Manisso
+
+clear
+
+git clone https://github.com/Manisso/Crips.git
+
+clear
+
+cd Crips
+
+clear
+
+sudo chmod +x install.sh
+
+clear
+
+./install.sh
+
+clear
